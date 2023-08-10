77a627e1d5e65788e87693c8469006aa564f793f
JD Tadlock <jt7903@gmail.com>
2023-08-10 14:37:20 -0400

    server graphql complete

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,3 +1,14 @@
-module.exports = {
+const Task = require('../models/Task');
 
+module.exports = {
+  Query: {
+    sayHello() {
+      return 'Hi There';
+    },
+    async getTasks() {
+      const tasks = await Task.find();
+
+      return tasks;
+    }
+  }
 }
